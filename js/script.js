15d8640e7357c9a4186955236cc81cbf23419e22
saurabhjn76 <saurabhjn76@gmail.com>
2017-10-23 00:23:02 +0530

    added neighborhood places

--- js/script.js
+++ js/script.js
@@ -4,19 +4,19 @@ var markers = [];
 function initMap() {
     // Constructor creates a new map - only center and zoom are required.
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 40.7413549, lng: -73.9980244},
+        center: {lat: 28.6127, lng: 77.2773},
         zoom: 13,
         mapTypeControl: false
     });
     // These are the real estate listings that will be shown to the user.
     // Normally we'd have these in a database instead.
     var locations = [
-        {title: 'Park Ave Penthouse', location: {lat: 40.7713024, lng: -73.9632393}},
-        {title: 'Chelsea Loft', location: {lat: 40.7444883, lng: -73.9949465}},
-        {title: 'Union Square Open Floor Plan', location: {lat: 40.7347062, lng: -73.9895759}},
-        {title: 'East Village Hip Studio', location: {lat: 40.7281777, lng: -73.984377}},
-        {title: 'TriBeCa Artsy Bachelor Pad', location: {lat: 40.7195264, lng: -74.0089934}},
-        {title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}}
+        {title: 'Akshardham', location: {lat: 28.6127, lng: 77.2773}},
+        {title: 'Worlds of Wonder', location: {lat: 28.5638, lng: 77.3261}},
+        {title: 'India Gate', location: {lat: 28.6129, lng: 77.2295}},
+        {title: 'Red Fort', location: {lat: 28.6562, lng: 77.2410}},
+        {title: 'Qutb Minar', location: {lat: 28.5244, lng: 77.1855}},
+        {title: 'Lotus Temple', location: {lat: 28.5535, lng: 77.2588}}
     ];
     var largeInfowindow = new google.maps.InfoWindow();
     // The following group uses the location array to create an array of markers on initialize.
